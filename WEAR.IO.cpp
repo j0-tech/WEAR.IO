f7232795c408b661257c119dd6b8acc94a6ce7e1
fortraan <fortraan1221@gmail.com>
2019-05-27 18:50:10 -0500

    Initial commit.

---
+++ WEAR.IO.cpp
@@ -0,0 +1,14 @@
+/*
+WEAR.IO source. For now, this is empty. In the future, however,
+any special behaviour for the WEAR.IO will be implemented here.
+
+This software is meant for the WEAR.IO wearable RIO.
+https://github.com/j0-tech/WEAR.IO/
+
+This software is licensed under the j0 tech open source license:
+https://github.com/j0-tech/license/blob/master/README.md
+
+Copyright (c) 2019 j0 tech
+*/
+
+#include "WEAR.IO.h"
---
+++ WEAR.IO.h
@@ -0,0 +1,38 @@
+/*
+WEAR.IO definitions header.
+
+This software is meant for the WEAR.IO wearable RIO.
+https://github.com/j0-tech/WEAR.IO/
+
+This software is licensed under the j0 tech open source license:
+https://github.com/j0-tech/license/blob/master/README.md
+
+Copyright (c) 2019 j0 tech
+*/
+
+#ifndef J0_TECH_WEAR_IO_H
+#define J0_TECH_WEAR_IO_H
+
+#include "Arduino.h"
+
+#define PWM0 3
+#define PWM1 5
+#define PWM2 6
+#define PWM3 9
+
+#define DIO0 2
+#define DIO1 4
+#define DIO2 7
+#define DIO3 8
+
+#define AIO0 A0
+#define AIO1 A1
+#define AIO2 A2
+#define AIO3 A3
+
+#define ENCODER0A 10
+#define ENCODER0B 11
+#define ENCODER1A 12
+#define ENCODER1B 13
+
+#endif
